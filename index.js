846d73d68807bd2590c016c7a649413e4a8cc78f
komal shrawankar <shrawankarkomal9@gmail.com>
2025-08-21 18:52:05 +0530

    Added root route and updated index.js

--- index.js
+++ index.js
@@ -9,10 +9,15 @@ connectDB();
 const app = express();
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("School Management API is running. Use /api/addSchool and /api/listSchools");
+});
+
 //routes
 app.use("/api",schoolRoutes);
 
 const PORT = process.env.PORT||5000;
+console.log("MONGO_URI:", process.env.MONGO_URI);
 app.listen(PORT,()=>{
     console.log(`server running on port ${PORT}`);
 });
